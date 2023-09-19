e1e4325dae79ba486b73dff0f7c0a14c14ed9771
adamnebylitsa <adam.nebylitsa@stonybrook.edu>
2023-09-19 16:31:47 -0400

    Add template for other methods and code outlines

--- trial_09_12_2023.py
+++ trial_09_12_2023.py
@@ -19,12 +19,12 @@ uri = uri_helper.uri_from_env(default='radio://0/70/2M/E7E7E7E710')
 logging.basicConfig(level=logging.ERROR)
 
 #initializing python dictionaries to hold different values of interest (will be exported to xls or csv)
-stabalizer_dict = {"stabilizer.roll":[], "stabilizer.pitch":[], "stabilizer.yaw":[]}
+stabilizer_dict = {"stabilizer.roll":[], "stabilizer.pitch":[], "stabilizer.yaw":[]}
 state_estimate_dict = {"stateEstimate.x":[],"stateEstimate.y":[],"stateEstimate.z":[]}
 range_dict = {"range.front":[],"range.back":[],"range.left":[],"range.right":[],"range.up":[]}
 
 #making list for easy access
-logging_dicts = [stabalizer_dict, state_estimate_dict, range_dict]
+logging_dicts = [stabilizer_dict, state_estimate_dict, range_dict]
 
 #Drone class will contain functions related to movement of drone
 class Drone():
@@ -42,13 +42,60 @@ class Drone():
     def forward(self, distance):
         self.mc.forward(distance)
         return
-    def left(self, angle):
+    def left(self, angle=90):
         self.mc.turn_left(angle_degrees=angle)
         return
-    def right(self, angle):
+    def right(self, angle=90):
         self.mc.turn_right(angle_degrees=angle)
         return
     
+    #TODO 
+    def is_Wall(self):
+        #determine if obstacle in front is the wall or something to go around
+        return False
+    
+    #TODO
+    def go_Around(self):
+        #turn right 
+        #check if obstacle in front if so break so that method is recalled
+        #go forward until nothing on the left side
+        #turn left
+        #go forward until nothing on left side
+        #turn left
+        #go forward same amount as first iteration
+        #turn right
+        return
+
+    #TODO 
+    def search_Land(self, distance=2):
+        #search for landing box using _____ method
+        #break when find box
+        return
+    
+    #TODO     
+    def find_center(self):
+        #at end of the box
+        #get that location
+        #find other edge of the box
+        #find length of box and go to center of it
+        #repeat for other direction
+        return
+    
+    #TODO
+    def go_to(self,distance):
+        #travels the distance needed avoiding obstacles
+        return
+
+    #TODO 
+    def search_Start(self):
+        #at assumed start position
+        #if not above box
+        #search for box using growing circles
+        #break when find box
+        return
+    
+
+
     def log_async(self, logconf):
         cf = self.scf.cf
         for log in logconf:
@@ -58,16 +105,23 @@ class Drone():
             log.stop()
 
     def log_callback(self, timestamp, data, logconf):
-        for log_dict in logging_dicts: #look at each dictionary for each value of interest
-            for param in log_dict: #look at each item in the dictionary
-                log_dict[param].append(data[param]) #add the value associated with that item to the list for that item
+        #for log_dict in logging_dicts: #look at each dictionary for each value of interest
+        if logconf.name == 'Stabilizer':
+            for param in logging_dicts[0]: #look at each item in the dictionary
+                logging_dicts[0][param].append(data[param]) #add the value associated with that item to the list for that item
+        elif logconf.name == 'Range':
+            for param in logging_dicts[2]: #look at each item in the dictionary
+                logging_dicts[2][param].append(data[param]) #add the value associated with that item to the list for that item
+        elif logconf.name == 'Position':
+            for param in logging_dicts[1]: #look at each item in the dictionary
+                logging_dicts[1][param].append(data[param]) #add the value associated with that item to the list for that item
 
         # for k in data:
         #     data_csv[k].append(data[k])
         #print('[%d][%s]: %s' % (timestamp, logconf.name, data))
         #print(type(data))
 
-#intialization required for asyn logger (look at documentation for this)
+#initialization required for asyn logger (look at documentation for this)
 def init_logs():
     lg_stab = LogConfig(name='Stabilizer', period_in_ms=10)
     lg_stab.add_variable('stabilizer.roll', 'float')
@@ -90,28 +144,58 @@ def init_logs():
 
 
 if __name__ == '__main__':
-    #initalization of drivers and logger (see documentation)
+    #initialization of drivers and logger (see documentation)
     cflib.crtp.init_drivers()
     logs = init_logs()
-
+    #for movement simplicity everything should be right angles
     with SyncCrazyflie(uri, cf=Crazyflie(rw_cache='./cache')) as scf:
         with MotionCommander(scf, default_height= .8) as mc:
             drone = Drone(scf, mc)
+            initial_pos = [state_estimate_dict['stateEstimate.x'][0],state_estimate_dict['stateEstimate.y'[0]]]
             drone.log_async(logs)
+            drone.rise(.2)
+            for i in range(10):
+                drone.forward(.2)
+                if range_dict["range.front"][-1]<500:
+                    break
             #drone.takeoff(0.1) 
-            time.sleep(1) ##hovering for 3 seconds
+            #time.sleep(1) ##hovering for 3 seconds
             #drone.forward(0.3)
             #drone.sleep(3)
             #drone.land()
+
+        #   TODO
+        #   Fly code 
+    #         at_end = False
+    #         while not at_end:
+    #             if range_dict["range.front"][-1]<500:
+    #                 if drone.is_wall():
+    #                     at_end=True
+    #                     break
+    #                 drone.go_Around()
+    #                 continue
+    #             drone.forward(.2)
+    #         drone.search_Land()
+    #         drone.find_center()
+    #         end_pos= [state_estimate_dict['stateEstimate.x'][-1],state_estimate_dict['stateEstimate.y'[-1]]]
+    # with SyncCrazyflie(uri, cf=Crazyflie(rw_cache='./cache')) as scf:
+    #     with MotionCommander(scf, default_height= .8) as mc:
+    #         drone = Drone(scf,mc)
+    #         distance=[initial_pos[0]-end_pos[0],initial_pos[1]-end_pos[1]]
+    #         drone.return_Start(distance)
+    #         drone.search_Start()
+    #         drone.find_center()
+    
+
     
     #creating pandas dataframes from logging dicts
-    stabalizer_df = pd.DataFrame(stabalizer_dict)
+    stabilizer_df = pd.DataFrame(stabilizer_dict)
     state_estimate_df = pd.DataFrame(state_estimate_dict)
     range_df = pd.DataFrame(range_dict)
 
     #writing each df to xlsx file
     with pd.ExcelWriter(path= "./data/test.xlsx", engine='xlsxwriter') as writer:
-        for index, df in enumerate([stabalizer_df, stabalizer_df, range_df], start=1):
+        for index, df in enumerate([stabilizer_df, stabilizer_df, range_df], start=1):
             df.to_excel(writer,sheet_name= str(index))
             
         # drone1.hover(1)
