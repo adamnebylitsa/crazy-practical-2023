7e09f8dd86589e7455964ddd4530d0ea7aa64ca6
adamnebylitsa <adam.nebylitsa@stonybrook.edu>
2023-09-27 14:42:36 -0400

    Add search_land to main.py

--- Main.py
+++ Main.py
@@ -132,7 +132,62 @@ class Drone():
         return True
 
     #TODO 
-    def search_Land(self, distance=2):
+    def search_Land(self, distance=2, found = False, at_l_wall = False, at_r_wall = False):
+        #assuming we start in the right corner of the section: -------------
+                                                   #                      * |
+                                                   #                        |       
+                                                   #
+        #while the landing area is not found, the drone will move left, then back, then right, then back .
+        # This will repeat until landing pad is found
+        #                        
+        while not found:
+            while not found and not at_l_wall:
+                if range_dict["range.left"][-1] < 500:
+                    if not self.is_Wall():
+                        self.go_Around()
+                        continue
+                    else:
+                        at_l_wall = True
+                        break
+
+                self.pc.left(.2)
+
+                if range_dict['range.up'][-1] < distance:
+                    found = True
+                    break
+
+            if not found:
+                if range_dict['range.back'][-1] < distance:
+                    if not self.is_Wall():
+                        self.go_Around('back')
+                self.pc.back()
+                        
+    
+
+            while not found and not at_r_wall:
+                if range_dict["range.right"][-1] < 500:
+                    if not self.is_Wall():
+                        self.go_Around('right')
+                        continue
+                    else:
+                        at_r_wall = True
+                        break
+
+                self.pc.right(.2)
+                if range_dict['range.up'][-1] < distance:
+                    found = True
+                    break
+                
+            
+            if not found:
+                if range_dict['range.back'][-1] < distance:
+                    if not self.is_Wall():
+                        self.go_Around('back')
+                self.pc.back(.2)
+
+        
+        
+
         #search for landing box using _____ method
         #break when find box
         return
